f61e69126ccea95c067f9736204f2dc83b1696f2
DARWIN CATUNTA <83663953+fdarwinjc@users.noreply.github.com>
2021-06-18 16:12:54 -0500

    Se logro extraer datos de wikipedia mediante Xpath 😃

--- nivel1_wikipedia.py
+++ nivel1_wikipedia.py
@@ -11,9 +11,24 @@ respuesta = requests.get(url, headers=encabezado)
 
 parser = html.fromstring(respuesta.text)
 
-ingles = parser.get_element_by_id("js-link-box-en")
+# Extraccion de datos por python para todos los idiomas
+# idiomas = parser.find_class('central-featured-lang')
+# for idioma in idiomas:
+#     print(idioma.text_content())
 
-print(ingles.text_content())
+# Extraccion de datos mediante Xpath para todos los idiomas
+idiomas = parser.xpath("//div[contains(@class, 'central-featured-lang')]//strong/text()")
+for idiomas in idiomas:
+    print(idiomas)
 
+# Extraccion de datos mediante Xpath para solo el idioma ingles
+# ingles = parser.xpath("//a[@id='js-link-box-en']/strong/text()")
+# print(ingles)
+
+# Extraccion de datos mediante python para solo el idioma ingles
+# ingles = parser.get_element_by_id("js-link-box-en")
+# print(ingles.text_content())
+
+# Test de la primera versión
 # print(respuesta.text)
 
